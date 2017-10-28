03c7707c6e778c89cde3e7f2e61b3649731e8774
Joshua Rosenberg <jrosen@msu.edu>
2017-10-28 01:43:06 -0400

    prepare for CRAN submission

--- R/helpers.R
+++ R/helpers.R
@@ -17,7 +17,7 @@ select_create_profiles <- function(df, ...){
 }
 
 # quiets concerns (notes) of R CMD check re: the vars that are evaluated using non-standard evaluation
-if (getRversion() >= "2.15.1") utils::globalVariables(c("matrix", "structure", "EEE", "EEI", "VVV", "est", "key", "model_names"))
+if (getRversion() >= "2.15.1") utils::globalVariables(c("matrix", "structure", "EEE", "EEI", "VVV", "est", "key", "model_names", "Covariance matrix structure", "n_profiles", "param_name", "posterior_prob", "profile", "val", "value", "var_name"))
 
 #' student questionnaire data with four variables from the 2015 PISA for students in the United States
 #'
