03e25f8d42f6b7abe68d8e6c5dc68cc76f108e0e
Joshua Rosenberg <jrosen@msu.edu>
2018-03-28 08:15:42 -0400

    fix typo

--- R/plot_profiles.R
+++ R/plot_profiles.R
@@ -10,7 +10,7 @@
 #' .5*(1/number of classes), then weighted likelihood bootstrapping is used to
 #' ensure that each case is represented in the bootstrap samples (see O’Hagan,
 #' Murphy, Scrucca, and Gormley, 2015).
-#' @param to_center whether to center the data before plottingtlmgr search --global --file "/framed.sty"
+#' @param to_center whether to center the data before plotting
 #' @param to_scale whether to scale the data before plotting
 #' @param plot_what whether to plot tibble or mclust output from estimate_profiles(); defaults to tibble
 #' @param plot_error_bars whether to plot error bars (representing the 95 percent confidence interval for the mean of each variable)
