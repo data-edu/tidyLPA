863dd2e825b229695168420a43c6d83945f0cf1f
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2019-02-13 22:14:55 -0500

    update tests

--- tests/testthat/test-compare-solutions.R
+++ tests/testthat/test-compare-solutions.R
@@ -1,6 +1,21 @@
 context("test-compare_solutions.R")
 
-test_that("compare_solutions works", {
-    x <- estimate_profiles(iris[, 1:4], 1:3)
+x <- estimate_profiles(iris[, 1:4], 1:3)
+
+test_that("compare_solutions works with iris data when we use compare_solutions()
+          outside of a pipe", {
     expect_s3_class(compare_solutions(x), "bestLPA")
+})
+
+pisaUSA_compare <-pisaUSA15[1:100, ] %>%
+    select(broad_interest, enjoyment, self_efficacy) %>%
+    single_imputation() %>%
+    estimate_profiles(1:3,
+                      variances = c("equal", "varying"),
+                      covariances = c("zero", "varying")) %>%
+    compare_solutions(statistics = c("AIC", "BIC"))
+
+test_that("compare_solutions works with PISA data when we use compare_solutions
+          in a pipe", {
+    expect_s3_class(pisaUSA_compare, "bestLPA")
 })
