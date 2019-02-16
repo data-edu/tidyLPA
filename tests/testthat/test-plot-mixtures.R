c9a93a00a76481ad89f042cf04ff33a8fb38a7b2
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2019-02-16 14:40:25 -0500

    update tests to pass checks

--- tests/testthat/test-plot-mixtures.R
+++ tests/testthat/test-plot-mixtures.R
@@ -3,12 +3,9 @@ context("test-plot-mixtures.R")
 results <- iris %>%
     subset(select = c("Sepal.Length", "Sepal.Width",
                       "Petal.Length", "Petal.Width")) %>%
-    estimate_profiles(1:3, package = "MplusAutomation")
-
+    estimate_profiles(1:3)
 density_plot <- plot_density(results, variables = "Petal.Length")
 
 test_that("density plot works",
-          skip_on_cran(),
-          skip_on_travis(),
-          expect_s3_class(density_plot, "ggplot")
+          expect_is(density_plot, "gg")
 )
