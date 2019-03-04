3a3a28aa4d6531712346d3f2f920224106a01b3f
Joshua Rosenberg <jrosen48@github.com>
2019-03-04 16:32:58 -0500

    remove example

--- tests/testthat/test-plot-profiles-mplus.R
+++ tests/testthat/test-plot-profiles-mplus.R
@@ -21,15 +21,3 @@ test_that("Parsing variable names throws error if variable names are not unique"
     names(df)[1:2] <- c("Sepal.Length1", "Sepal.Length2")
     expect_error(estimate_profiles(df, n_profiles = 3, package = "MplusAutomation"))
 })
-
-test_that("plot profiles works with imputed and scaled data using Mplus", {
-    skip_on_cran()
-    skip_on_travis()
-    pisa_profiles_plot_mplus <- pisaUSA15[1:100, ] %>%
-        select(broad_interest, enjoyment, self_efficacy) %>%
-        single_imputation() %>%
-        scale() %>%
-        estimate_profiles(3, package = "MplusAutomation") %>%
-        plot_profiles()
-    expect_s3_class(estimate_profiles(pisa_profiles_plot, "ggplot"))
-})
