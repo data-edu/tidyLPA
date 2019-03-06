7c588973d14e4b6d063d7ea1c25864431ba22390
Joshua Rosenberg <jrosen48@github.com>
2019-03-06 14:54:07 -0500

    update tests

--- tests/testthat/test-estimate-profiles-mplus.R
+++ tests/testthat/test-estimate-profiles-mplus.R
@@ -73,35 +73,65 @@ test_that("LogLik values are as expected for model type 6", {
 test_that("fit stats are the same for both mplus and mclust", {
     skip_on_cran()
     skip_on_travis()
-    tab <- tibble::tribble( # these are from mclust
+
+    tab_mclust <- tibble::tribble(
         ~stat,         ~val,
-        "Model",            2,
+        "Model",            1,
         "Classes",            2,
-        "LogLik",      -98.973,
-        "AIC",      207.946,
-        "AWE",   245.681359,
-        "BIC",  215.2746795,
-        "CAIC",  220.2746795,
-        "CLC",      199.868,
-        "KIC",      215.946,
-        "SABIC",  199.6875335,
-        "ICL", -215.7374133,
-        "Entropy",        0.961,
-        "prob_min",        0.987, # from mplus - .857
-        "prob_max",        0.994,
-        "n_min",        0.125,
-        "n_max",        0.875,
-        "BLRT_val",         6.81,
-        "BLRT_p",            1 # from Mplus: .2178
+        "LogLik", -271.2943387,
+        "AIC",  556.5886774,
+        "AWE",  610.4725311,
+        "BIC",  566.8488288,
+        "CAIC",  573.8488288,
+        "CLC",  544.2251264,
+        "KIC",  566.5886774,
+        "SABIC",  545.0268243,
+        "ICL", -569.7289846,
+        "Entropy",  0.818224466,
+        "prob_min",   0.95731999,
+        "prob_max",  0.960443609,
+        "n_min",      0.46875,
+        "n_max",      0.53125,
+        "BLRT_val",  22.53901766,
+        "BLRT_p",   0.00990099
     )
 
-    m_cars_mplus <- estimate_profiles(mtcars[, "mpg"],
+    # for benchmarking
+    # createMixtures(classes = 2, filename_stem = "mtcars-mpg-hp", rdata = mtcars[, c("mpg", "hp")])
+    # runModels(target = "mtcars-mpg-hp_2_class.inp")
+    # o <- readModels("mtcars-mpg-hp_2_class.out")
+
+    tab_benchmark <- tibble::tribble( # these are from benchmarking code above
+        ~stat,         ~val,
+        "Model",            1,
+        "Classes",            2,
+        "LogLik",      -271.292,
+        "AIC",      556.584,
+        "BIC",  566.844,
+        "Entropy",        0.819,
+        "BLRT_val",         22.544
+    )
+
+    m_cars_mplus <- estimate_profiles(mtcars[, c("mpg", "hp")],
                                       n_profiles = 2,
-                                      models = 2,
+                                      models = 1,
                                       package = "MplusAutomation")
-    m_cars_mclust <- estimate_profiles(mtcars[, "mpg"],
-                                      n_profiles = 2,
-                                      models = 2)
-    expect_equal(m_cars_mplus$model_2_class_2$fit, tab$val, tolerance = 1)
-    expect_equal(m_cars_mclust$model_2_class_2$fit, tab$val, tolerance = 1)
+    m_cars_mclust <- estimate_profiles(mtcars[, c("mpg", "hp")],
+                                       n_profiles = 2,
+                                       models = 1)
+
+    expect_equal(as.vector(m_cars_mplus$model_1_class_2$fit), tab_mclust$val, tolerance = .05)
+    expect_equal(as.vector(m_cars_mclust$model_1_class_2$fit), tab_mclust$val, tolerance = .05)
+
+    m_cars_mplus_benchmark_stats <- tibble(stat = names(m_cars_mplus$model_1_class_2$fit),
+                                           val = m_cars_mplus$model_1_class_2$fit)
+
+    m_cars_clust_benchmark_stats <- tibble(stat = names(m_cars_mclust$model_1_class_2$fit),
+                                           val = m_cars_mclust$model_1_class_2$fit)
+
+    m_cars_mplus_benchmark_stats <- m_cars_mplus_benchmark_stats[m_cars_mplus_benchmark_stats$stat %in% tab_benchmark$stat, ]
+    m_cars_mclust_benchmark_stats <- m_cars_mplus_benchmark_stats[m_cars_mplus_benchmark_stats$stat %in% tab_benchmark$stat, ]
+
+    expect_equal(m_cars_mplus_benchmark_stats$val, tab_benchmark$val, tolerance = .05)
+    expect_equal(m_cars_mclust_benchmark_stats$val, tab_benchmark$val, tolerance = .05)
 })
