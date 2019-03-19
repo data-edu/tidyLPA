6a8e60eb6e4a052d885d5380756e66bae1e5384c
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2019-03-19 13:51:34 -0400

    add test back

--- tests/testthat/test-compare-solutions.R
+++ tests/testthat/test-compare-solutions.R
@@ -6,3 +6,15 @@ test_that("compare_solutions works with iris data when we use compare_solutions(
           outside of a pipe", {
     expect_s3_class(compare_solutions(x), "bestLPA")
 })
+
+pisaUSA_compare <-pisaUSA15[1:100, ] %>%
+    single_imputation() %>%
+    estimate_profiles(1:3,
+                      variances = c("equal", "varying"),
+                      covariances = c("zero", "varying")) %>%
+    compare_solutions(statistics = c("AIC", "BIC"))
+
+test_that("compare_solutions works with PISA data when we use compare_solutions
+          in a pipe", {
+    expect_s3_class(pisaUSA_compare, "bestLPA")
+})
