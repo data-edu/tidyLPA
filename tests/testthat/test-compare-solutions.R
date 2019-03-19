26a3233b006e52791f855ef7b8400ab6540a2dd1
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2019-03-19 13:39:09 -0400

    update

--- tests/testthat/test-compare-solutions.R
+++ tests/testthat/test-compare-solutions.R
@@ -1,6 +1,6 @@
 context("test-compare_solutions.R")
 
-x <- estimate_profiles(iris[, 1:4], 1:3)
+x <- estimate_profiles(iris[, 1:4], n_profiles = 1:3)
 
 test_that("compare_solutions works with iris data when we use compare_solutions()
           outside of a pipe", {
